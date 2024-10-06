e7388e5f479b74761172f3b3ffd92c54f023682e
liyang <guzhoudiaoxue0706@163.com>
2024-10-06 14:36:28 +0800

    train

--- hw1/src/train.py
+++ hw1/src/train.py
@@ -129,9 +129,10 @@ def compute_language_modeling_loss(
     Hint: Think about what are the groundtruth labels for next token prediction.
     """
 
-    labels = ...
-    loss = ...
-    return
+    labels = input_ids[:,1:]
+    logits = logits[:,:-1,:]
+    loss =  F.cross_entropy(logits.view(-1, logits.size(-1)), labels.view(-1))
+    return loss
 
 
 def train(
@@ -161,20 +162,22 @@ def train(
 
     for step in (pbar := trange(num_training_steps)):
         t0 = time.time()
-        lr = ...
+        lr = lr_schedule(step)
         set_lr(optimizer, lr)
 
         for _ in range(grad_accumulation_steps):
             # TODO: sample a batch, generate logits and compute loss
-            input_ids = ...
+            input_ids = next(batch_sampler)
             with autocast:
-                logits = ...
-            loss = ...
+                logits = model(input_ids)
+            loss = compute_language_modeling_loss(input_ids,logits)
             (loss / grad_accumulation_steps).backward()
             loss_f = loss.item()
             losses.append(loss_f)
 
         # TODO: update the model using the accumulated gradients
+        optimizer.step()
+        optimizer.zero_grad()
         loss_mean = np.mean(losses).item()
 
         FLOPs_per_step = (
